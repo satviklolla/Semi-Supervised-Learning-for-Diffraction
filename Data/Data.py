2632337c7d6694c6a168ee7d9c0a28ee113a7d8f
satviklolla <satviklolla@gmail.com>
2021-10-29 12:23:06 -0400

    Upload Files

--- Data/Data.py
+++ Data/Data.py
@@ -29,17 +29,22 @@ class DiffractionDataset(Dataset):
         idx = torch.randint(0,len(self.data),(batch_size,))
         return self.data[idx]
     
-    def _get_categorical(self, label):
-        return self.mapping[label]
-    
     def plot(self, idx=0):
         assert idx<len(self.data), "Index out of range, please input an index between 0 and {}".format(len(self))
-        data=self.data[idx].cpu().numpy()
-        xrange=np.arange(3, data.shape[0]*0.05+3, 0.05)
+        data=self.data[idx].cpu().numpy().flatten()
+        xrange=np.arange(3, data.shape[-1]*0.05+3, 0.05)
         fig = plt.plot(xrange,data)
         plt.xlabel("2\u03B8")
         plt.ylabel("Normalized Intensity")
         str_label=self.mapping[int(self.labels[idx].item())]
-        plt.title("{}: {}".format(self.categorical,str_label)) 
+        plt.title("{}: {}".format(self.categorical,str_label))
         return fig
 
+    def _unencode_labels(self, labels):
+        return np.asarray([self.mapping[int(i.item())] for i in labels])
+    def compare(self, prediction):
+        prediction=self._unencode_labels(prediction)
+        labels=self._unencode_labels(self.labels)
+        print("{: >3}{: >20}{: >20}".format("Index", "True Label", "Prediction"))
+        for i in range(len(prediction)):
+            print("{: >3} {: >20} {: >20}".format(i, labels[i], prediction[i]))
